9ba86acf0d3be3175f3c366e67cc66833b383e1c
Joseph Quested <josephquested@gmail.com>
2017-04-10 10:47:26 +1200

    state machine hooked up to input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	StateMachine sm;
+
+	void Start ()
+	{
+		sm = GetComponent<StateMachine>();
+	}
+
+	void Update ()
+	{
+		HorizontalAxis();
+		VerticalAxis();
+	}
+
+	void HorizontalAxis ()
+	{
+		sm.ReceiveHorizontalAxis(Input.GetAxis("Horizontal"));
+	}
+
+	void VerticalAxis ()
+	{
+		sm.ReceiveVerticalAxis(Input.GetAxis("Vertical"));
+	}
+}
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum States { Break, Idle, Moving };
+
+// 0: Break
+// 1: Idle
+// 2: Moving
+
+public class StateMachine : MonoBehaviour {
+
+	Movement movement;
+
+	public States state = States.Idle;
+
+	public float horizontal;
+	public float vertical;
+
+	// SYSTEM //
+
+	void Start ()
+	{
+		movement = GetComponent<Movement>();
+	}
+
+	void Update ()
+	{
+		UpdateMovementState();
+	}
+
+	// INPUTS //
+
+	public void ReceiveHorizontalAxis (float _horizontal)
+	{
+		horizontal = _horizontal;
+	}
+
+	public void ReceiveVerticalAxis (float _vertical)
+	{
+		vertical = _vertical;
+	}
+
+	// STATE CONTROL //
+
+	void Transition (States newState)
+	{
+		state = newState;
+	}
+
+	bool CanTransition (States newState)
+	{
+		switch (state)
+		{
+			case States.Break:
+				return new int[] { 0, 1, 2 }.Contains((int)newState);
+
+			case States.Idle:
+				return new int[] { 0, 1, 2 }.Contains((int)newState);
+
+			case States.Moving:
+				return new int[] { 0, 1, 2 }.Contains((int)newState);
+
+			default:
+				return false;
+		}
+	}
+
+	// MOVEMENT //
+
+	void UpdateMovementState ()
+	{
+		if (horizontal != 0 || vertical != 0)
+		{
+			if (CanTransition(States.Moving) || state == States.Moving)
+			{
+				Transition(States.Moving);
+				movement.ReceiveInput(horizontal, vertical);
+			}
+		}
+		else
+		{
+			if (CanTransition(States.Idle) || state == States.Idle)
+			{
+				Transition(States.Idle);
+			}
+		}
+	}
+
+	bool CanMove ()
+	{
+		int[] moveableStates = new int[] { 1, 2 };
+		return moveableStates.Contains((int)state);
+	}
+}
