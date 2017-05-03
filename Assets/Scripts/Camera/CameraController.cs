5f1c33c7f53376468ced585aa17951ab80f1b5d7
Joseph Quested <josephquested@gmail.com>
2017-05-04 11:18:06 +1200

    basic chatacter controller moving

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,23 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// SYSTEM //
+
+	public Transform target;
+
+	void Update ()
+	{
+		if (target != null)
+		{
+			FollowTarget();
+		}
+	}
+
+	void FollowTarget ()
+	{
+		transform.position = new Vector3(target.position.x, target.position.y + 11, target.position.z - 10);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	// SYSTEM //
+
+	PlayerController playerController;
+
+	void Start ()
+	{
+		playerController = GetComponent<PlayerController>();
+	}
+
+	void FixedUpdate ()
+	{
+		UpdateMovement();
+	}
+
+	// MOVEMENT //
+
+	void UpdateMovement ()
+	{
+		playerController.ReceiveMovement(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+}
