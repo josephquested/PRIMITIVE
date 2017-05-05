cfefb461133344c6f9675a0a21bb8e7bee804329
Joseph Quested <josephquested@gmail.com>
2017-05-05 15:21:44 +1200

    add is player script

---
+++ Assets/Scripts/Player/IsPlayer.cs
@@ -0,0 +1,20 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsPlayer : MonoBehaviour {
+
+	// SYSTEM //
+
+	void Start ()
+	{
+		GetComponentInChildren<AudioListener>().enabled = true;
+		PlayerInput playerInput = gameObject.AddComponent<PlayerInput>() as PlayerInput;
+		RotateToCursor rotateToCursor = gameObject.AddComponent<RotateToCursor>() as RotateToCursor;
+	}
+
+	void Update ()
+	{
+
+	}
+}
