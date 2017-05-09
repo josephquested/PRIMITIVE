b332b362d8fa40d88e366345652cb171a781a2c5
Joseph Quested <josephquested@gmail.com>
2017-05-09 12:57:36 +1200

    ghoul moving on nav mesh

---
+++ Assets/Scripts/Enemy/GhoulController.cs
@@ -0,0 +1,30 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhoulController : MonoBehaviour {
+
+	// SYSTEM //
+
+	public Transform goal;
+
+	void Start ()
+	{
+	  agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		agent.speed = Random.Range(minSpeed, maxSpeed);
+	}
+
+	void Update ()
+	{
+		agent.destination = goal.position;
+	}
+
+	// NAVIGATION //
+
+	NavMeshAgent agent;
+
+	public float minSpeed;
+	public float maxSpeed;
+
+}
